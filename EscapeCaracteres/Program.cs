956eb2c7052472946a74e8af1349339471cc078e
LARRYSALSA <75591878+LARRYICLOUD@users.noreply.github.com>
2023-10-29 16:05:23 -0500

    Agregar archivos de proyecto.

---
+++ EscapeCaracteres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeCaracteres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Secuencia de caracteres
+
+            //Secuencia de escape "Nueva Linea" \n
+            // \t hace lo que haria la tecla tabulacion
+            //\r retorno de carro
+
+            Console.WriteLine("YO SOY UN PROGRAMADOR\n Y EXPERTO EN REDES EXCELENTE\n Y RECONOCIDO");
+            Console.WriteLine("Así es la vida,\t así de irónica,\r\nunos llevan a cuesta la cruz que otro debe cargar." +
+                "\r\nAsí es la vida; así de lógica,\r\ncada quien tiene alguna historia que hablar o callar,\r\nasí es la vida, así...................");
+
+
+
+
+
+        }
+    }
+}
